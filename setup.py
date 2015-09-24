080ac050d127b41def6aeb4285832df3dcaca486
oms064 <otokonoko064@gmail.com>
2015-09-24 00:07:24 -0500

    Era presiona enter en input.

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ import os
 os.system("git pull")
 print("Ultima modificacion:\n")
 os.system("git log -1 --pretty=%B")
-input("\n\nPresione una tecla para continuar...")
+input("\n\nPresione enter para continuar...")
