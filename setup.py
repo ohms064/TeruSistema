e33d64bd31c76b0e8ebec543ff92237c3bfdd4f0
oms064 <otokonoko064@gmail.com>
2015-09-24 00:03:44 -0500

    ya

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
 os.system("git pull")
-print("Última modificación:\n")
+print("Ultima modificacion:\n")
 os.system("git log -1 --pretty=%B")
 input("\n\nPresione una tecla para continuar...")
